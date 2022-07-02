a0a1fb92f4dcc7a638e778de5fcce8380a184241
Denrage <denrage@gmail.com>
2022-07-02 13:26:01 +0200

    only draw markers in bounds of minimap

--- src/Denrage.AdventureModule/UserInterface/MapMarkerContainer.cs
+++ src/Denrage.AdventureModule/UserInterface/MapMarkerContainer.cs
@@ -4,11 +4,20 @@ using Denrage.AdventureModule.Helper;
 using Denrage.AdventureModule.Services;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
 
 namespace Denrage.AdventureModule.UserInterface
 {
     public class MapMarkerContainer : Control
     {
+
+        private const int MAPWIDTH_MAX = 362;
+        private const int MAPHEIGHT_MAX = 338;
+        private const int MAPWIDTH_MIN = 170;
+        private const int MAPHEIGHT_MIN = 170;
+        private const int MAPOFFSET_MIN = 19;
+
         private readonly DrawObjectService drawObjectService;
 
         public Texture2D Texture { get; set; }
@@ -24,14 +33,35 @@ namespace Denrage.AdventureModule.UserInterface
         protected override void Paint(SpriteBatch spriteBatch, Rectangle bounds)
         {
             var markers = this.drawObjectService.GetDrawObjects<Libs.Messages.Data.MapMarker>();
+            var miniMapRectangle = new Rectangle(0, 0, 0, 0);
+            if (!GameService.Gw2Mumble.UI.IsMapOpen)
+            {
+                var offsetWidth = this.GetOffset(GameService.Gw2Mumble.UI.CompassSize.Width, MAPWIDTH_MAX, MAPWIDTH_MIN, 40);
+                var offsetHeight = this.GetOffset(GameService.Gw2Mumble.UI.CompassSize.Height, MAPHEIGHT_MAX, MAPHEIGHT_MIN, 40);
+
+                miniMapRectangle.Location = GameService.Gw2Mumble.UI.IsCompassTopRight
+                    ? new Point(GameService.Graphics.SpriteScreen.ContentRegion.Width - GameService.Gw2Mumble.UI.CompassSize.Width - offsetWidth + 1, 1)
+                    : new Point(GameService.Graphics.SpriteScreen.ContentRegion.Width - GameService.Gw2Mumble.UI.CompassSize.Width - offsetWidth,
+                                              GameService.Graphics.SpriteScreen.ContentRegion.Height - GameService.Gw2Mumble.UI.CompassSize.Height - offsetHeight - 40);
+
+                miniMapRectangle.Size = new Point(GameService.Gw2Mumble.UI.CompassSize.Width + offsetWidth,
+                                    GameService.Gw2Mumble.UI.CompassSize.Height + offsetHeight);
+            }
+
             foreach (var item in markers)
             {
                 var location = MapUtils.ContinentToMapScreen(item.Position.ToVector());
                 location = new Vector2(location.X - 20, location.Y - 10);
-                spriteBatch.Draw(this.Texture, new Rectangle((int)location.X, (int)location.Y, 40, 20), Color.White);
-            }
 
+                if (GameService.Gw2Mumble.UI.IsMapOpen || miniMapRectangle.Contains(location))
+                {
+                    spriteBatch.Draw(this.Texture, new Rectangle((int)location.X, (int)location.Y, 40, 20), Color.White);
+                }
+            }
         }
+
+        private int GetOffset(float curr, float max, float min, float val)
+            => (int)Math.Round((curr - min) / (max - min) * (val - MAPOFFSET_MIN) + MAPOFFSET_MIN, 0);
 
     }
 }
